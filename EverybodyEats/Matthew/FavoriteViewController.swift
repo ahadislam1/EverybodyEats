fc7afe4a7fd0c08da7605e740f0ec8f46fe65361
Matthew Ramos <mmramos309@gmail.com>
2020-04-21 13:30:45 -0400

    Added changes to collection view delegate

--- EverybodyEats/Matthew/FavoriteViewController.swift
+++ EverybodyEats/Matthew/FavoriteViewController.swift
@@ -15,10 +15,10 @@ class FavoriteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
-        view = favoriteView
         favoriteView.collectionView.dataSource = self
         favoriteView.collectionView.delegate = self
         favoriteView.collectionView.register(ItemCell.self, forCellWithReuseIdentifier: "itemCell")
+        view = favoriteView
     }
 
 
@@ -39,8 +39,20 @@ extension FavoriteViewController: UICollectionViewDataSource {
     
 }
 
-extension FavoriteViewController: UICollectionViewDelegate {
+extension FavoriteViewController: UICollectionViewDelegateFlowLayout {
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        1
+        4
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 100, height: 100)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+
+    
+    
 }
