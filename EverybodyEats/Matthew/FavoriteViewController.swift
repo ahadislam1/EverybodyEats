c6fc3b9f8ca3c5b696b1a0aa6734b23bbe80c2f4
Matthew Ramos <mmramos309@gmail.com>
2020-04-20 20:33:14 -0400

    Created view for items and set up constraints for collection view

--- EverybodyEats/Matthew/FavoriteViewController.swift
+++ EverybodyEats/Matthew/FavoriteViewController.swift
@@ -9,21 +9,14 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
+    
+    let favoriteView = FavoriteView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        view = favoriteView
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
