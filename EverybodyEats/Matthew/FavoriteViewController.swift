0df78824968726e327dd3440c901016fe59871ba
Matthew Ramos <mmramos309@gmail.com>
2020-04-20 21:31:38 -0400

    Set up item cell and xib, began setup of collection view data source and delegation

--- EverybodyEats/Matthew/FavoriteViewController.swift
+++ EverybodyEats/Matthew/FavoriteViewController.swift
@@ -16,7 +16,31 @@ class FavoriteViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
         view = favoriteView
+        favoriteView.collectionView.dataSource = self
+        favoriteView.collectionView.delegate = self
+        favoriteView.collectionView.register(ItemCell.self, forCellWithReuseIdentifier: "itemCell")
     }
 
 
+}
+
+extension FavoriteViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as? ItemCell else {
+            fatalError("Could not downcast to item cell")
+        }
+        return cell
+    }
+    
+    
+}
+
+extension FavoriteViewController: UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
 }
