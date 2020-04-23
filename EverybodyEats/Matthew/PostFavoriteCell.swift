392684f6861d6f3f01f5f82b2b7f562e8d230ac9
Matthew Ramos <mmramos309@gmail.com>
2020-04-23 11:48:51 -0400

    Setup kingfisher for images in cell, setup allergen indicators using enum

--- EverybodyEats/Matthew/PostFavoriteCell.swift
+++ EverybodyEats/Matthew/PostFavoriteCell.swift
@@ -16,11 +16,22 @@ class PostFavoriteCell: UICollectionViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var datePostedLabel: UILabel!
+    @IBOutlet weak var allergenIndicatorIV: UIImageView!
     
     public func configureCell(post: Post) {
+        imageView.contentMode = .scaleAspectFill
+        imageView.kf.setImage(with: URL(string: post.imageURL))
         likesLabel.text = String(post.like.count)
         userNameLabel.text = post.userHandle
         captionLabel.text = post.caption
         datePostedLabel.text = post.createdDate.dateString()
+        switch post.allergen.first {
+        case Allergen.peanuts.rawValue:
+            allergenIndicatorIV.image = UIImage(named: "peanut")
+        case Allergen.milk.rawValue:
+            allergenIndicatorIV.image = UIImage(named: "milk")
+        default:
+            allergenIndicatorIV.image = UIImage(named: "navImage")
+        }
     }
 }
