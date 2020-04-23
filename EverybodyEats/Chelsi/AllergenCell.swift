a783351653a5fe861069f70a181c90aaa4e1e86e
Chelsi Christmas <c.christmas95@gmail.com>
2020-04-23 05:19:34 -0400

    minimal set up for Allergen Cell

--- EverybodyEats/Chelsi/AllergenCell.swift
+++ EverybodyEats/Chelsi/AllergenCell.swift
@@ -11,7 +11,42 @@ import UIKit
 class AllergenCell: UICollectionViewCell {
     
     
-    public func configureCell(){
+    public lazy var allergyNameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "hi"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    
+    private func commonInit() {
+        setupAllergyNameLabel()
+    }
+    
+    private func setupAllergyNameLabel() {
+        addSubview(allergyNameLabel)
+        allergyNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            allergyNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            allergyNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            allergyNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 10)
+        ])
+    }
+    
+    public func configureCell(allergy: Allergen) {
+        allergyNameLabel.text = allergy.rawValue
+    }
+    public func configureNameLabel() {
         
+        allergyNameLabel.backgroundColor = .systemGray
     }
 }
